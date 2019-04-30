378cf652ec7bd9ef204709c6bf229bd62991009d
Meliwen <kelly_cool@hotmail.fr>
2019-04-30 10:53:56 +0200

    Modification du fichier index.js : le titre et l'artiste s'affichent de manière automatique dans la console

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+/*$.ajax({
+
+   url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
+
+   dataType : 'jsonp'
+
+}).done(function(musiques) {
+
+   console.log(musiques);
+
+    document.querySelector('#results').innerHTML =
+
+        musiques.data.map(m => m.title).join('<br>');
+
+});*/
+
+
 $.ajax({
     url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
-    dataType : 'jsonp'
+    dataType : 'jsonp' 
 }).done(function(musiques) {
- 
-console.log(musiques);
 
-document.querySelector('#results').innerHTML =
-    musiques.data.map(m => m.title).join('<br>');
- });
+for (var i = 0; i < musiques.data.length; i++) {
+        console.log(musiques.data[i].title)
+        console.log(musiques.data[i].artist.name)
+    }
+});
+
+
+
