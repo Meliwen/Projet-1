99f114e4672bb999d572fa6fa3032eb9c545666c
Meliwen <kelly_cool@hotmail.fr>
2019-05-03 09:31:16 +0200

    Mise à jour des fichiers, rajout de css

---
+++ data.js
@@ -0,0 +1,39 @@
+{
+    "data": [{
+                "id": "548348632",
+                "readable": true,
+                "title": "Lucky You",
+                "title_short": "Lucky You",
+                "title_version": "",
+                "link": "https://www.deezer.com/track/548348632",
+                "duration": "244",
+                "rank": "893293",
+                "explicit_lyrics": true,
+                "explicit_content_lyrics": 1,
+                "explicit_content_cover": 1,
+                "preview": "https://cdns-preview-c.dzcdn.net/stream/c-ce814e88e289e9c11353ab0a976fa742-4.mp3",
+                "artist": {
+                    "id": "13",
+                    "name": "Eminem",
+                    "link": "https://www.deezer.com/artist/13",
+                    "picture": "https://api.deezer.com/artist/13/image",
+                    "picture_small": "https://e-cdns-images.dzcdn.net/images/artist/0707267475580b1b82f4da20a1b295c6/56x56-000000-80-0-0.jpg",
+                    "picture_medium": "https://e-cdns-images.dzcdn.net/images/artist/0707267475580b1b82f4da20a1b295c6/250x250-000000-80-0-0.jpg",
+                    "picture_big": "https://e-cdns-images.dzcdn.net/images/artist/0707267475580b1b82f4da20a1b295c6/500x500-000000-80-0-0.jpg",
+                    "picture_xl": "https://e-cdns-images.dzcdn.net/images/artist/0707267475580b1b82f4da20a1b295c6/1000x1000-000000-80-0-0.jpg",
+                    "tracklist": "https://api.deezer.com/artist/13/top?limit=50",
+                    "type": "artist"
+                },
+                "album": {
+                    "id": "72000342",
+                    "title": "Kamikaze",
+                    "cover": "https://api.deezer.com/album/72000342/image",
+                    "cover_small": "https://e-cdns-images.dzcdn.net/images/cover/bf74fc764097630ba58782ae79cfbee6/56x56-000000-80-0-0.jpg",
+                    "cover_medium": "https://e-cdns-images.dzcdn.net/images/cover/bf74fc764097630ba58782ae79cfbee6/250x250-000000-80-0-0.jpg",
+                    "cover_big": "https://e-cdns-images.dzcdn.net/images/cover/bf74fc764097630ba58782ae79cfbee6/500x500-000000-80-0-0.jpg",
+                    "cover_xl": "https://e-cdns-images.dzcdn.net/images/cover/bf74fc764097630ba58782ae79cfbee6/1000x1000-000000-80-0-0.jpg",
+                    "tracklist": "https://api.deezer.com/album/72000342/tracks",
+                    "type": "album"
+                },
+                "type": "track"
+            },
